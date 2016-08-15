ff937ee572d7f4112a63616e2d258e3d2b5c827d
Sergio <oliveserg@gmail.com>
2016-08-15 18:48:54 +0000

    creating a user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   root "movies#index"
+
+  resources :users, only: [:new, :create]
 end
